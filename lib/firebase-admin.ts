7f5af1053ee58f4bca6a0a24cf8ae865cb7216af
Darshna Shingavi <darshnashingavi21@gmail.com>
2026-05-23 10:27:29 +0530

    fix firebase admin initialization

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,15 +1,19 @@
 import admin from "firebase-admin";
 
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-};
+const projectId = process.env.FIREBASE_PROJECT_ID;
+const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
 
-if (!admin.apps.length) {
+if (!admin.apps.length && projectId && clientEmail && privateKey) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId,
+      clientEmail,
+      privateKey,
+    }),
   });
 }
 
-export const adminDb = admin.firestore();
+export const adminDb = admin.apps.length
+  ? admin.firestore()
+  : null;
